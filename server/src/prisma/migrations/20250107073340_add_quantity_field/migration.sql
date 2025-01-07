ef82c80ba8ad204d4a10a8662787a733b9af1018
attaln <azleidszhea225@gmail.com>
2025-01-07 15:17:44 +0700

    feat: Add quantity field to listing and transaction models with default value

---
+++ server/src/prisma/migrations/20250107073340_add_quantity_field/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `listing` ADD COLUMN `quantity` INTEGER NOT NULL DEFAULT 1;
---
+++ server/src/prisma/migrations/20250107080938_add_quantity_to_transaction/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `transaction` ADD COLUMN `quantity` INTEGER NOT NULL DEFAULT 1;
